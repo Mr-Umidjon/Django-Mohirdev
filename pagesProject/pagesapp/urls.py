8bedcb4f90c3e8c33950cc2600d221dd3dc038e6
Umidjon <mrumidjon2020@gmail.com>
2022-11-10 08:25:49 +0500

    add new templates

--- pagesProject/pagesapp/urls.py
+++ pagesProject/pagesapp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView
+from .views import HomePageView, AboutPageView, ContactView, SellsView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name="home"),
-    path('about/', AboutPageView.as_view(), name="home"),
+    path('about/', AboutPageView.as_view(), name="About"),
+    path('contact/', ContactView.as_view(), name="About"),
+    path('sells/', SellsView.as_view(), name='Sells'),
 ]
--- pagesProject/pagesapp/views.py
+++ pagesProject/pagesapp/views.py
@@ -6,5 +6,14 @@ from django.views.generic import TemplateView
 class HomePageView(TemplateView):
     template_name = 'home.html'
 
+
 class AboutPageView(TemplateView):
     template_name = 'about.html'
+
+
+class ContactView(TemplateView):
+    template_name = 'contacts.html'
+
+
+class SellsView(TemplateView):
+    template_name = 'sells.html'
