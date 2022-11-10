2de4b5d42ff6711a6c7bb609253f47d4c529d5bd
Umidjon <mrumidjon2020@gmail.com>
2022-11-10 09:00:49 +0500

    add new templates

--- pagesProject/pagesapp/urls.py
+++ pagesProject/pagesapp/urls.py
@@ -3,7 +3,7 @@ from .views import HomePageView, AboutPageView, ContactView, SellsView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name="home"),
-    path('about/', AboutPageView.as_view(), name="About"),
-    path('contact/', ContactView.as_view(), name="About"),
+    path('about/', AboutPageView.as_view(), name="about"),
+    path('contact/', ContactView.as_view(), name="contacts"),
     path('sells/', SellsView.as_view(), name='Sells'),
 ]
