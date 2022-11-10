aa72d1e272a64064e0dc186411adf93c02178642
Umidjon <mrumidjon2020@gmail.com>
2022-11-10 14:44:17 +0500

    Pages Project

--- pagesProject/pagesapp/tests.py
+++ pagesProject/pagesapp/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
+
 
 # Create your tests here.
+class SimpleTests(SimpleTestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_page_status_code(self):
+        response = self.client.get('/about/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_contact_page_status_code(self):
+        response = self.client.get('/contact/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_sells_page_status_code(self):
+        response = self.client.get('/sells/')
+        self.assertEqual(response.status_code, 200)
