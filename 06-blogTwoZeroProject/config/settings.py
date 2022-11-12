77a9a33bdc324607f5f399b2550bdc638ac7288e
Umidjon <mrumidjon2020@gmail.com>
2022-11-13 01:27:07 +0500

    Blog 2.0 Project

--- 06-blogTwoZeroProject/config/settings.py
+++ 06-blogTwoZeroProject/config/settings.py
@@ -35,7 +35,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'accounts',
+    'pages',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,5 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
